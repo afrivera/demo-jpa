d69e2de8203da18d8c38366653588eafbfab363b
afrivera <andresrfelip@hotmail.com>
2022-09-27 20:58:29 -0500

    realizando un insert para la tabla categoria

--- src/main/java/net/afrivera/JpaDemoApplication.java
+++ src/main/java/net/afrivera/JpaDemoApplication.java
@@ -1,5 +1,6 @@
 package net.afrivera;
 
+import net.afrivera.model.Categoria;
 import net.afrivera.repository.CategoriasRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,13 +20,18 @@ public class JpaDemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// este metodo es para que la app corra como una app cli
-		System.out.println(repo);
+		// System.out.println(repo);
 		guardar();
 	}
 
 	private void guardar(){
 		// System.out.println("Insertando un registro");
+		Categoria cat = new Categoria();
+		cat.setNombre("Finanzas");
+		cat.setDescripcion("Trabajos relacionados con finanzas y contabilidad");
 
+		repo.save(cat);
+		System.out.println(cat);
 	}
 
 	private void eliminar(){
